21533deff40c8d7e402ae86c3163538f951c0d54
mrinoybanerjee <mrinoybanerjee@gmail.com>
2024-02-15 23:26:11 -0500

    Adding streamlit app

---
+++ src/app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import os
+from dotenv import load_dotenv
+from model import RAGModel
+
+# Load the environment variables
+load_dotenv()
+
+MONGODB_CONNECTION_STRING = os.getenv("MONGODB_URI")
+MONGODB_DATABASE = os.getenv("MONGODB_DATABASE")
+MONGODB_COLLECTION = os.getenv("MONGODB_COLLECTION")
+API_TOKEN = os.getenv("API_TOKEN")
+
+# Initialize the RAGModel with your MongoDB and Replicate settings
+rag_model = RAGModel(
+    mongo_connection_string=MONGODB_CONNECTION_STRING,
+    mongo_database=MONGODB_DATABASE,
+    mongo_collection=MONGODB_COLLECTION,
+    api_token=API_TOKEN
+)
+
+# Define your query
+query = st.text_input("Message AyurBot: ")
+
+# Perform semantic search and generate an answer
+answer = rag_model.generate_answer(query)
+
+# Print the generated answer
+
+st.write("Generated Answer:", answer)
+
